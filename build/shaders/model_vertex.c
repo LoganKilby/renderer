a2db29bed2ad7a26c6c895ba60de342897bbe24e
Logan Kilby <logankilby@gmail.com>
2021-08-06 17:05:47 -0500

    Update model_vertex.c

--- build/shaders/model_vertex.c
+++ build/shaders/model_vertex.c
@@ -2,7 +2,6 @@
 layout (location = 0) in vec3 aPos;
 layout (location = 1) in vec3 aNormal;
 layout (location = 2) in vec2 aTexCoord;
-
 // NOTE: OUT VARIABLES MUST HAVE CORRESPONDING VARIABLES IN THE FRAGMENT SHADER
 //       BOTH OF THE VARIABLES NEED TO HAVE THE SAME NAME
 out vec2 TexCoord;
