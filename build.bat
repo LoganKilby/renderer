6777aa2a6dd94caa399fc07a608af51d89337ffd
Logan Kilby <logankilby@gmail.com>
2021-08-19 15:16:50 -0500

    Update build.bat

--- build.bat
+++ build.bat
@@ -2,7 +2,7 @@
 
 ctime -begin timing.ctm
 
-set opts=-FC -GR- -EHa- -nologo -Zi -DASSERTIONS_ENABLED=1 -DUNIFORM_ASSERTIONS_ENABLED=0 /std:c++17 /EHsc
+set opts=-FC -GR- -EHa- -nologo -Zi -DASSERTIONS_ENABLED=1 -DUNIFORM_ASSERTIONS_ENABLED=0 /EHsc
 set code=%cd%
 pushd build
 cl %opts% %code%\glfw_main.cpp -Ferenderer kernel32.lib gdi32.lib user32.lib opengl32.lib  /link /LIBPATH:..\lib glfw3dll.lib glfw3dll.exp glew32.lib assimp-vc141-mt.lib
